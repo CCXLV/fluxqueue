9894f8e5581326edd722c9874bda1b8468333806
Giorgi Merebashvili <abracadabragio12@gmail.com>
2026-01-18 21:17:39 +0400

    Add TODOs (#18)
    * Remove TODO comment
    
    * Add TODOs.md

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,10 @@
-from flask import Flask
 from fastapi import FastAPI
 
 from tests.tasks import print_name, send_hello
 
-# app = Flask(__name__)
 fastapp = FastAPI()
-
-# @app.route("/<name>")
-# async def index(name: str):
-#     await send_hello(name)
-#     return {"message": "Printed name"}, 200
-
 
 @fastapp.get("/{name}")
 async def _index(name: str):
     await send_hello(name, email="test@test.com")
     return {"message": "Printed name"}, 200
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
