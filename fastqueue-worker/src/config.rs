6cd0f03ffaa8a6450abdaa4fb14b8fc070cf9887
Giorgi Merebashvili <abracadabragio12@gmail.com>
2026-01-23 23:42:00 +0400

    Add more TODOs (#21)
    * Start checking failed tasks
    
    * Add a todo

--- fastqueue-worker/src/config.rs
+++ fastqueue-worker/src/config.rs
@@ -4,8 +4,24 @@ pub mod redis_keys {
     pub const PROCESSING: &str = "fastqueue:processing";
     pub const FAILED: &str = "fastqueue:failed";
     pub const DEAD: &str = "fastqueue:dead";
-}
 
-pub fn get_task_key(queue_name: &str) -> String {
-    format!("{}:{}", redis_keys::TASK_QUEUE, queue_name)
+    pub fn get_workers_key(queue_name: &str) -> String {
+        format!("{}:{}", WORKERS, queue_name)
+    }
+
+    pub fn get_queue_key(queue_name: &str) -> String {
+        format!("{}:{}", TASK_QUEUE, queue_name)
+    }
+
+    pub fn get_processing_key(queue_name: &str, worker_id: &str) -> String {
+        format!("{}:{}:{}", PROCESSING, queue_name, worker_id)
+    }
+
+    pub fn get_failed_key(queue_name: &str) -> String {
+        format!("{}:{}", FAILED, queue_name)
+    }
+
+    pub fn get_dead_key(queue_name: &str) -> String {
+        format!("{}:{}", DEAD, queue_name)
+    }
 }
