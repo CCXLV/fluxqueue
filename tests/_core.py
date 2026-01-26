e1eeef7ac491edf43e9f4771848f56f323243399
Giorgi Merebashvili <abracadabragio12@gmail.com>
2026-01-27 00:07:36 +0400

    Remove unrelated 'tests'

--- tests/_core.py
+++
@@ -1,6 +0,0 @@
-from fastqueue import FastQueue, AsyncFastQueue
-
-REDIS_URL = "redis://127.0.0.1:6380"
-
-fastqueue = FastQueue(redis_url=REDIS_URL)
-async_fastqueue = AsyncFastQueue(redis_url=REDIS_URL)
