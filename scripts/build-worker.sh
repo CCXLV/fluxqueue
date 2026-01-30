f9562eb589c9efbcaf497d662f8828c7ebca72a7
Giorgi Merebashvili <mereba2627@gmail.com>
2026-01-30 22:00:50 +0400

    Add initial worker release workflow file

---
+++ scripts/build-worker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+TARGET=$1
+
+if [ -z "$TARGET" ]; then
+  echo "Target missing"
+  exit 1
+fi
+
+
+cargo build --release --target "$TARGET"
+
+mkdir -p dist
+cp "target/$TARGET/release/fastqueue-worker" "dist/fastqueue-worker-$TARGET"
