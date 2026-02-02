b06d88fdfb2910eaf01b2da16430f209845b653b
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-02 18:32:50 +0400

    Remove update subcommand

--- crates/fastqueue-worker/src/main.rs
+++ crates/fastqueue-worker/src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::{Parser, Subcommand};
-use self_update::cargo_crate_version;
 use tracing::{error, info, warn};
 
 #[derive(Parser, Debug)]
@@ -54,9 +53,7 @@ enum Commands {
             help = "Saves dead tasks in Redis that have used all their retries yet still failed. Can be useful for debugging."
         )]
         save_dead_tasks: bool,
-    },
-    #[command(about = "Update the worker")]
-    Update,
+    }
 }
 
 #[tokio::main]
@@ -107,29 +104,6 @@ async fn main() -> Result<()> {
 
             info!("Worker has shut down successfully.");
         }
-        Commands::Update => {
-            tokio::task::spawn_blocking(|| update())
-                .await
-                .context("Update task panicked")??;
-        }
     }
-    Ok(())
-}
-
-fn update() -> Result<()> {
-    let github_token = std::env::var("GITHUB_TOKEN").context("GITHUB_TOKEN is not set")?;
-
-    let status = self_update::backends::github::Update::configure()
-        .repo_owner("ccxlv")
-        .repo_name("fastqueue")
-        .auth_token(&github_token)
-        .bin_name("github")
-        .show_download_progress(true)
-        .current_version(cargo_crate_version!())
-        .build()
-        .context("Failed to build the new version")?
-        .update()
-        .context("Failed to update the new worker ")?;
-    println!("Update status: `{}`!", status.version());
     Ok(())
 }
