d2257b2c0bd9921b11f64d303244b1d4e8353f4e
Giorgi Merebashvili <abracadabragio12@gmail.com>
2026-02-05 17:04:13 +0400

    Add tests for checking enqueued tasks (#44)

---
+++ tests/test_tasks.py
@@ -0,0 +1,34 @@
+import pytest
+
+from .main import fluxqueue, redis_client
+
+
+def test_sync_task():
+    @fluxqueue.task()
+    def say_hello(name: str):
+        print("Hello ", name)
+
+    result = say_hello("George")
+    redis_result = redis_client.lrange("fluxqueue:queue:default", 0, -1)
+    redis_string = redis_result[0].decode("utf-8", errors="ignore")  # type: ignore
+
+    assert result == None
+    assert "George" in redis_string
+
+    redis_client.flushdb()
+
+
+@pytest.mark.asyncio
+async def test_async_task():
+    @fluxqueue.task()
+    async def async_hello(name: str):
+        print("Async Hello ", name)
+
+    result = await async_hello("Async George")
+    redis_result = redis_client.lrange("fluxqueue:queue:default", 0, -1)
+    redis_string = redis_result[0].decode("utf-8", errors="ignore")  # type: ignore
+
+    assert result == None
+    assert "Async George" in redis_string
+
+    redis_client.flushdb()
