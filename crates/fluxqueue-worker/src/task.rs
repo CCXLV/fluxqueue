675b1ef3591983f72d0f0aa5e01b53671e4270f6
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-11 14:38:30 +0400

    Update way of running async task functions (#83)
    * Update way of running async task functions
    
    * Remove commented lines

--- crates/fluxqueue-worker/src/task.rs
+++ crates/fluxqueue-worker/src/task.rs
@@ -1,7 +1,10 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use pyo3::prelude::*;
+use pyo3_async_runtimes::tokio::into_future;
 use std::collections::HashMap;
+use std::pin::Pin;
 use std::sync::{Arc, RwLock};
+use tokio::sync::{mpsc, oneshot};
 
 pub struct TaskRegistry {
     tasks: Arc<RwLock<HashMap<String, Arc<Py<PyAny>>>>>,
@@ -25,5 +28,54 @@ impl TaskRegistry {
     pub fn get(&self, name: &str) -> Option<Arc<Py<PyAny>>> {
         let tasks = self.tasks.read().ok()?;
         tasks.get(name).cloned()
+    }
+}
+
+type PyResponse = Pin<Box<dyn Future<Output = PyResult<Py<PyAny>>> + Send>>;
+
+struct TaskRequest {
+    func: Py<PyAny>,
+    resp_tx: oneshot::Sender<PyResult<PyResponse>>,
+}
+
+pub struct PythonDispatcher {
+    tx: mpsc::Sender<TaskRequest>,
+}
+
+impl PythonDispatcher {
+    pub fn new() -> Self {
+        let logical_cores = num_cpus::get();
+        let (tx, mut rx) = mpsc::channel::<TaskRequest>(logical_cores * 2);
+
+        std::thread::spawn(move || {
+            Python::attach(|py| {
+                let dispatcher = async move {
+                    while let Some(req) = rx.recv().await {
+                        let res = Python::attach(|py| into_future(req.func.into_bound(py)));
+
+                        let _ = req.resp_tx.send(res.map(|f| Box::pin(f) as PyResponse));
+                    }
+                    Ok(())
+                };
+
+                pyo3_async_runtimes::tokio::run(py, dispatcher).expect("Python loop failed");
+            });
+        });
+
+        Self { tx }
+    }
+
+    pub async fn execute(&self, func: Py<PyAny>) -> Result<Py<PyAny>> {
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        self.tx
+            .send(TaskRequest { func, resp_tx })
+            .await
+            .map_err(|_| anyhow!("Dispatcher channel closed"))?;
+
+        let py_fut = resp_rx.await??;
+
+        let result = py_fut.await?;
+        Ok(result)
     }
 }
