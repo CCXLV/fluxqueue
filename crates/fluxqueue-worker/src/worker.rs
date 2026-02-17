69b0dda5811dbe31d56a6e7c43bb281ec8ff22a2
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-17 12:02:59 +0400

    Add todo in worker

--- crates/fluxqueue-worker/src/worker.rs
+++ crates/fluxqueue-worker/src/worker.rs
@@ -58,6 +58,7 @@ pub async fn run_worker(
         let executor_id = Arc::clone(&executor_ids[i]);
         let shutdown = shutdown.clone();
         let task_registry = Arc::clone(&task_registry);
+        // TODO: Add a way to let the user choose between having 1 event loop per tokio task or just a single event loop in a worker.
         let python_dispatcher = Arc::new(PythonDispatcher::new());
 
         redis_client
