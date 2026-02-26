8435fa6b6805c227cc9b15a09db7c9704a99cea1
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-26 21:44:53 +0400

    Fix run-tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,12 +2,12 @@
 
 set -euxo pipefail
 
-# Running Python tests
-uv run pytest tests/
+source .venv/bin/activate
 
 # Setting up LD_LIBRARY_PATH environment variable
 # https://pyo3.rs/v0.28.2/building-and-distribution.html#dynamically-embedding-the-python-interpreter
 LIB_DIR=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 export LD_LIBRARY_PATH="$LIB_DIR:${LD_LIBRARY_PATH:-}"
 
-uv run cargo test --workspace --locked
+pytest tests/
+cargo test --workspace --locked
