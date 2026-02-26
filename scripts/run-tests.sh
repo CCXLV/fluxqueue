fc4d10d7316b3f93f9cfb939b48195013ec63abd
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-26 21:50:43 +0400

    Fix run-tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,8 +6,15 @@ source .venv/bin/activate
 
 # Setting up LD_LIBRARY_PATH environment variable
 # https://pyo3.rs/v0.28.2/building-and-distribution.html#dynamically-embedding-the-python-interpreter
+OS="$(uname)"
 LIB_DIR=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
-export LD_LIBRARY_PATH="$LIB_DIR:${LD_LIBRARY_PATH:-}"
+
+if [[ "$OS" == "Darwin" ]]; then
+    export DYLD_LIBRARY_PATH="$LIB_DIR:${DYLD_LIBRARY_PATH:-}"
+else
+    export LD_LIBRARY_PATH="$LIB_DIR:${LD_LIBRARY_PATH:-}"
+fi
 
 pytest tests/
+
 cargo test --workspace --locked
