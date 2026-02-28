93e9ebe451a772c8eee0555ec6ac8d6a6f74e28d
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-28 18:47:05 +0400

    Fix type checker error in __init__

--- python/fluxqueue/__init__.py
+++ python/fluxqueue/__init__.py
@@ -1,4 +1,4 @@
-from ._core import __version__ as __version__
+from ._core import __version__ as __version__  # type: ignore
 from .client import FluxQueue as FluxQueue
 from .context import Context as Context
 from .models import TaskMetadata as TaskMetadata
